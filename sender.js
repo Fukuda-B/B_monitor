d6e30705d748ec6f7b207738428df8293b367f1b
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2021-01-15 20:51:26 +0900

    Implemented websocket using node.js
    In development... Can test a bit in local.

--- sender.js
+++ sender.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+var fps = 10; // Frame rate / (s)
+var videoQuo = 0.5 // Video Quality (Max: 1.0)
 var constraints = {
   audio: true,
   video: {
@@ -10,19 +13,35 @@ var constraints = {
   //video: {facingMode: "user"} // Select FrontCamera
   //video: {facingMode: {exact: "enviroment"}} // Select RearCamera
 };
-
-if (typeof navigator.mediaDevices.getUserMedia !== 'function') {
-  console.log("not support getUserMedia()");
-}
-
 var video = document.getElementById('main_video');
 var info = document.getElementById('info');
 var video_c = document.getElementById('cap_video');
 var ctx = video_c.getContext("2d");
+var receive_video = document.getElementById('receive_video');
 
+
+/* Websocket connection */
+var socket = new WebSocket('ws://127.0.0.1:1011');
+// start
+socket.addEventListener('open', function(e) {
+  console.log('Connected!');
+});
+// receive
+socket.addEventListener('message', function(e) {
+  receive_video.src = e.data;
+});
+// send
+function sender(data) {
+  socket.send(data);
+}
+
+
+/* Get camera data */
+if (typeof navigator.mediaDevices.getUserMedia !== 'function') {
+  console.log("not support getUserMedia()");
+}
 navigator.mediaDevices.getUserMedia(constraints)
 .then(function(mediaStream) {
-
   video.srcObject = mediaStream;
   video.onloadedmetadata = function(e) {
     console.log('Size: '+video.videoWidth+'x'+video.videoHeight);
@@ -35,9 +54,12 @@ navigator.mediaDevices.getUserMedia(constraints)
 })
 .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
 
+
+/* Update canvas frame */
 function update_canvas() {
   ctx.drawImage(video, 0, 0, video_c.width, video_c.height);
-  setTimeout(update_canvas, 100);
+  var dataURI = video_c.toDataURL("image/jpeg", videoQuo);
+  sender(dataURI); // send img data
+  setTimeout(update_canvas, 1000/fps);
 }
 
-update_canvas();
