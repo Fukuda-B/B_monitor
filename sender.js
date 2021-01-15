2efa218c8a9de9dc7e84cf4afdd2b3705d3333f9
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2021-01-15 19:22:57 +0900

    Add canvas (for capture)

--- sender.js
+++ sender.js
@@ -15,15 +15,29 @@ if (typeof navigator.mediaDevices.getUserMedia !== 'function') {
   console.log("not support getUserMedia()");
 }
 
+var video = document.getElementById('main_video');
+var info = document.getElementById('info');
+var video_c = document.getElementById('cap_video');
+var ctx = video_c.getContext("2d");
+
 navigator.mediaDevices.getUserMedia(constraints)
 .then(function(mediaStream) {
-  var video = document.getElementById('main_video');
-  var info = document.getElementById('info');
+
   video.srcObject = mediaStream;
   video.onloadedmetadata = function(e) {
     console.log('Size: '+video.videoWidth+'x'+video.videoHeight);
     info.innerHTML = 'CaptureSize: '+video.videoWidth+'x'+video.videoHeight;
     video.play();
+
+    video_c.height = video.videoHeight;
+    video_c.width = video.videoWidth;
   };
 })
 .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
+
+function update_canvas() {
+  ctx.drawImage(video, 0, 0, video_c.width, video_c.height);
+  setTimeout(update_canvas, 100);
+}
+
+update_canvas();
