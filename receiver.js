070b409716dfef2eda9db339731890513c77dbbb
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2021-01-19 10:49:10 +0900

    D
    I figured I could play the two videos and copy the screen to the canvas for smooth playback. However, it did not work. D(Dead)

--- receiver.js
+++ receiver.js
@@ -1,10 +1,15 @@
 'use strict';
 
 var counter = 0; // video receive counter
-var recInt = 500; // Recode Interval
+var recInt = 1000; // Recode Interval
+var fps = 20; // Frame rate / (s)
 
 var blob = [];
 var receive_video = document.getElementById('receive_video');
+var receive_video2 = document.getElementById('receive_video2');
+var video_c = document.getElementById('main_video');
+var ctx = video_c.getContext("2d");
+
 
 /* Websocket connection */
 var socket = new WebSocket('ws://127.0.0.1:1011');
@@ -16,14 +21,21 @@ socket.addEventListener('open', function(e) {
 socket.addEventListener('message', function(e) {
   blob = new Blob([blob, e.data], {type: 'video/x-matroska;codecs=avc1,opus'});
   // blob = new Blob([blob, e.data], {type: 'video/webm'});
-  receive_video.src = URL.createObjectURL(blob);
-  console.log(blob);
   counter++;
+  update_canvas(counter, blob); // copy video to canvas
+  console.log(blob);
 });
 
-function update_canvas() {
-  if (counter*recInt/1000 > 1) {
-    receive_video.currentTime = counter*recInt/1000;
+function update_canvas(counter, blob) {
+  if (counter > 1) {
+    if (counter%2) {
+      var attVideo = receive_video;
+    } else {
+      var attVideo = receive_video2;
+    }
+    attVideo.currentTime = (counter-1)*recInt/1000;
+    attVideo.src = URL.createObjectURL(blob);
+    attVideo.play();
+    ctx.drawImage(attVideo, 0, 0, attVideo.videoWidth, attVideo.videoHeight);
   }
-  receive_video.play();
 }
